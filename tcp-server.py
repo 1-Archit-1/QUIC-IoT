778744e76811832571ff278933abe3a04dcfdf40
1-Archit-1 <architsengupta@gmail.com>
2025-04-16 20:47:16 -0400

    TCP logging

--- tcp-server.py
+++ tcp-server.py
@@ -5,7 +5,12 @@ from datetime import datetime
 from collections import deque
 import asyncio
 import traceback
+import time
+import logging
 
+# Set up logging
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s', filename='tcp_server.log')
 class TCPIMUServer:
     def __init__(self, host='0.0.0.0', port=5555):
         self.host = host
@@ -15,6 +20,10 @@ class TCPIMUServer:
         self.message_queue =  asyncio.Queue()
         self.buffer_size = 4096 
         self.client_buffers = {}
+        self.accel_count = 0
+        self.gyro_count = 0
+        self.start_time = time.time()
+        self.last_log = time.time()
 
     async def process_accel_data(self,data):
         """Process accelerometer data."""
@@ -34,11 +43,22 @@ class TCPIMUServer:
                     await asyncio.sleep(0)
                     continue
                 message = self.message_queue.get_nowait()
-                if message.startswith("ACCEL:"):                    
+                now = time.time()
+                if message.startswith("ACCEL:"):          
+                    self.accel_count += 1          
                     await self.process_accel_data(message)
                 elif message.startswith("GYRO:"):
+                    self.gyro_count += 1
                     await self.process_gyro_data(message)
-                self.message_queue.task_done()
+                print(now-self.last_log)
+                if now - self.last_log >= 5:
+                    total_time = now - self.start_time
+                    print(f"[TCP] Accel rate: {self.accel_count / total_time:.2f} msgs/sec | "
+                        f"Gyro rate: {self.gyro_count / total_time:.2f} msgs/sec over {total_time:.1f}s")
+                    logging.info(f"[TCP] Accel rate: {self.accel_count / total_time:.2f} msgs/sec | "
+                                 f"Gyro rate: {self.gyro_count / total_time:.2f} msgs/sec over {total_time:.1f}s")
+                    self.last_log = now
+                    self.message_queue.task_done()
             except queue.Empty:
                 continue
 
